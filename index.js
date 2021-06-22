f02027ba1ec0c00278f233d790c559bd7ac9ab64
khaleekahmad <khaleek.ahmad@gmail.com>
2021-06-22 14:34:25 +0530

    port are same now

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
 
 
-// const http = require('http');
+const http = require('http');
 
-// const hostname = '0.0.0.0';
-// const port = 3333;
+const hostname = '0.0.0.0';
+const port = 3333;
 
-// const MY_MESSAGE = process.env.MY_MESSAGE
+const MY_MESSAGE = process.env.MY_MESSAGE
 
-// const server = http.createServer((req, res) => {
-//   res.statusCode = 200;
-//   res.setHeader('Content-Type', 'text/plain');
-//   res.end(MY_MESSAGE);
-// });
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end(MY_MESSAGE);
+});
 
 
 
-// server.listen(port, hostname, () => {
-//   console.log(`Server running at http://${hostname}:${port}/`);
-// });
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
 /////////////////////////////////////////////////
 
 const express = require('express');

5ed56a52321e0282a8d2d75e965df28eafcc4627
khaleekahmad <khaleek.ahmad@gmail.com>
2021-06-22 12:05:43 +0530

    remove db connection

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const port1 = 5001;
 const app = express();
 const mongoose = require('mongoose');
 
-const secret = require('./config');
+//const secret = require('./config');
 app.use('/', (req,res)=>res.send("Hello World."));
 app.listen(port1,()=>console.log("Hello Terminal."));
 
 
-// connect node to mongodb
-mongoose.connect(secret.database, {useNewUrlParser: true,useUnifiedTopology: true}).then(
-    reaolve=>console.log("Connected."),
-    reject=>console.log('Make sure the database server is running ' + reject));
+// // connect node to mongodb
+// mongoose.connect(secret.database, {useNewUrlParser: true,useUnifiedTopology: true}).then(
+//     reaolve=>console.log("Connected."),
+//     reject=>console.log('Make sure the database server is running ' + reject));

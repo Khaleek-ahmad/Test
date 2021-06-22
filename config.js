ebcc591cd2fd05e477153bddf23c5f71aa775af8
khaleekahmad <khaleek.ahmad@gmail.com>
2021-06-22 11:15:52 +0530

    add database

---
+++ config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    //database: 'mongodb://localhost:27017/shopper',
+    database: 'mongodb://root:rMjv2GhGVF6tGjtP@shopper-ypt87d92krfgox6l-svc.qovery.io:27017/shopper',
+    port: process.env.PORT || 3000,
+    secretKey: "supersecret",
+    facebook: {
+        clientID: process.env.FACEBOOK_ID || '<facebook_id>',
+        clientSecret: process.env.FACEBOOK_SECRET || '<facebook_secret_key>',
+        profileFields: ['emails', 'displayName'],
+        callbackURL: 'https://localhost/auth/facebook/callback'
+    }
+};
--- index.js
+++ index.js
@@ -2,5 +2,12 @@ const express = require('express');
 const port1 = 5001;
 const app = express();
 
+const secret = require('./config');
 app.use('/', (req,res)=>res.send("Hello World."));
 app.listen(port1,()=>console.log("Hello Terminal."));
+
+
+// connect node to mongodb
+mongoose.connect(secret.database, {useNewUrlParser: true,useUnifiedTopology: true}).then(
+    reaolve=>console.log("Connected."),
+    reject=>console.log('Make sure the database server is running ' + reject));

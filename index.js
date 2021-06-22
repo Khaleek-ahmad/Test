195ec5eec6ff84145cb1fcf27c81f5c83550b83a
khaleekahmad <khaleek.ahmad@gmail.com>
2021-06-22 14:27:22 +0530

    add get

--- index.js
+++ index.js
@@ -1,31 +1,36 @@
-const http = require('http');
-
-const hostname = '0.0.0.0';
-const port = 3333;
-
-const MY_MESSAGE = process.env.MY_MESSAGE
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end(MY_MESSAGE);
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
-
-// const express = require('express');
-// const port1 = 5001;
-// const app = express();
-// const mongoose = require('mongoose');
-
-// //const secret = require('./config');
-// app.use('/', (req,res)=>res.send("Hello World."));
-// app.listen(port1,()=>console.log("Hello Terminal."));
 
 
-// // // connect node to mongodb
-// // mongoose.connect(secret.database, {useNewUrlParser: true,useUnifiedTopology: true}).then(
-// //     reaolve=>console.log("Connected."),
-// //     reject=>console.log('Make sure the database server is running ' + reject));
+// const http = require('http');
+
+// const hostname = '0.0.0.0';
+// const port = 3333;
+
+// const MY_MESSAGE = process.env.MY_MESSAGE
+
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/plain');
+//   res.end(MY_MESSAGE);
+// });
+
+
+
+// server.listen(port, hostname, () => {
+//   console.log(`Server running at http://${hostname}:${port}/`);
+// });
+/////////////////////////////////////////////////
+
+const express = require('express');
+const port1 = 3333;
+const app = express();
+
+
+//const secret = require('./config');
+app.get('/', (req,res)=>res.send("Hello World."));
+app.listen(port1,()=>console.log("Hello Terminal."));
+
+
+// // connect node to mongodb
+// mongoose.connect(secret.database, {useNewUrlParser: true,useUnifiedTopology: true}).then(
+//     reaolve=>console.log("Connected."),
+//     reject=>console.log('Make sure the database server is running ' + reject));

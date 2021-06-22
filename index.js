3e9ea5cf78c9944ddbae0e0e80d22b55b4934c05
khaleekahmad <khaleek.ahmad@gmail.com>
2021-06-22 15:13:23 +0530

    removed commented code

--- index.js
+++ index.js
@@ -1,25 +1,4 @@
 
-
-// const http = require('http');
-
-// const hostname = '0.0.0.0';
-// const port = 3331;
-
-// //const MY_MESSAGE = process.env.MY_MESSAGE
-
-// const server = http.createServer((req, res) => {
-//   res.statusCode = 200;
-//   res.setHeader('Content-Type', 'text/plain');
-//   res.end("Hello Qovery");
-// });
-
-
-
-// server.listen(port, hostname, () => {
-//   console.log(`Server running at http://${hostname}:${port}/`);
-// });
-/////////////////////////////////////////////////
-
 const express = require('express');
 const port1 = 3331;
 const app = express();
@@ -29,8 +8,3 @@ const secret = require('./config');
 app.get('/', (req,res)=>res.send("Hello World."));
 app.listen(port1,()=>console.log("Hello Terminal."));
 
-
-// // connect node to mongodb
-// mongoose.connect(secret.database, {useNewUrlParser: true,useUnifiedTopology: true}).then(
-//     reaolve=>console.log("Connected."),
-//     reject=>console.log('Make sure the database server is running ' + reject));
